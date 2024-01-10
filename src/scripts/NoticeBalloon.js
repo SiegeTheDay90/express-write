39b42edb17638e0e7e1244f11aaa30f55740fe26
cjsmi <ClarenceSmith90@gmail.com>
2024-01-09 22:56:29 -0500

    add report button to error notifications

--- src/scripts/NoticeBalloon.js
+++ src/scripts/NoticeBalloon.js
@@ -1,12 +1,23 @@
 class NoticeBalloon{
     constructor(container, type="notice", message){
+        debugger;
         this.type = type;
         this.parent = container;
         this.balloon = document.createElement("div");
+        this.balloon.style.position = "relative";
         this.balloon.classList.add("balloon")
         this.balloon.classList.add(this.type === "error" ? "text-danger" : "text-info")
         this.balloon.classList.add(type);
         this.balloon.innerText = message;
+        if(this.type === "error"){
+            debugger;
+            const reportButton = document.createElement("span");
+            reportButton.innerHTML = `<a href="/bugreport?error=one">Report</a>`
+            reportButton.style.position = "absolute";
+            reportButton.style.right = "7px";
+            reportButton.style.bottom = "4px";
+            this.balloon.appendChild(reportButton);
+        }
         this.closeButton = document.createElement("span");
         this.balloon.appendChild(this.closeButton);
         this.closeButton.classList.add("close-button");
