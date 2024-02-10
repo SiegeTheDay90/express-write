bcc7a6561f74e28b4c78c1bbc4c9992d59c94f50
cjsmi <ClarenceSmith90@gmail.com>
2024-02-10 18:53:44 -0500

    unhandled_error renders json instead of html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -104,14 +104,15 @@ class ApplicationController < ActionController::Base
     end
 
     def unhandled_error(error)
-        if request.accepts.first.html?
-            raise error
-        else
-            @message = "#{error.class} - #{error.message}"
-            @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
-            render 'api/errors/internal_server_error', status: :internal_server_error
-            logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
-        end
+        # if request.accepts.first.html?
+        #     raise error
+        # else
+        @message = "#{error.class} - #{error.message}"
+        @stack = Rails::BacktraceCleaner.new.clean(error.backtrace)
+        # render 'api/errors/internal_server_error', status: :internal_server_error
+        render json: {ok: false, status: @message}
+        logger.error "\n#{@message}:\n\t#{@stack.join("\n\t")}\n"
+        # end
     end
 
     def user_params
