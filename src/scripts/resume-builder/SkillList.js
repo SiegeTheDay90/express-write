250db8c81f55ff83b367458af52cd2cf150e8576
cjsmi <ClarenceSmith90@gmail.com>
2024-03-06 17:31:24 -0500

    forms complete with preview; TODO: output docx or PDF

--- src/scripts/resume-builder/SkillList.js
+++ src/scripts/resume-builder/SkillList.js
@@ -2,37 +2,73 @@ import { useRef, useState } from 'react'
 import React from 'react';
 import SkillListItem from './SkillListItem';
 
-function SkillList(){
+function SkillList({resume: [resume, setResume]}){
 
-  const [formItems, setFormItems] = useState([]);
+  const formItems = resume.skills;
   const [newSkill, setNewSkill] = useState("");
   const [count, setCount] = useState(0);
+  console.log(formItems);
 
   function newSkillChange(e){
-    // console.log(e);
     setNewSkill(e.target.value);
   }
 
   function AddItem(e){
     if(newSkill.trim()){
       setNewSkill("");
-      setCount(count + 1);
-      const remove = () => setFormItems(items => items.filter(el => el.num !== count));
-      
-      const item = <SkillListItem key={count} value={newSkill.trim()} remove={remove}/>
+      const item = newSkill;
   
-  
-      setFormItems(formItems => formItems.concat([{ num: count, item }]))
+      setResume(prevState => ({
+        ...prevState,
+        skills: prevState.skills.concat([item])
+      }))
     }
   };
+
+  function remove(e){
+    const idx = parseInt(e.currentTarget.dataset.idx);
+    // debugger
+    setResume(prevState => ({
+      ...prevState,
+      skills: prevState.skills.slice(0, idx).concat(prevState.skills.slice(idx+1))
+    }))
+  }
+
+  function move(e){
+    const dir = e.currentTarget.dataset.dir === "up" ? -1 : 1;
+    const idx = parseInt(e.currentTarget.dataset.idx);
+
+    console.log(dir);
+    console.log(idx);
+
+    setResume(prevState => {
+      const items = [...prevState.skills];
+      const holder = prevState.skills[idx];
+      items[idx] = items[idx + dir];
+      items[idx + dir] = holder;
+      return {
+        ...prevState,
+        skills: items
+      }
+    })
+  }
 
     return (
       <>
         <ul>
-          {formItems.map((el) => el.item)}
+          {formItems.map((el, idx) => {
+            return <li className="mb-1" key={idx}>
+              <span>{el} &nbsp;</span>
+              <div className="btn-group" role="group" >
+                <button onClick={move} data-idx={idx} data-dir="up" className={`btn btn-secondary ${idx == 0 ? 'disabled' : ''}`}><i className="fa-solid fa-arrow-up"></i></button>
+                <button onClick={move} data-idx={idx} data-dir="down" className={`btn btn-secondary ${idx == resume.skills.length-1 ? 'disabled' : ''}`}><i className="fa-solid fa-arrow-down"></i></button>
+                <button onClick={remove} data-idx={idx} className={`btn btn-danger`}><i className="fa-solid fa-trash-can"></i></button>
+              </div>
+            </li>
+          })}
         </ul>
-        <div className='col-sm-3 d-flex'>
-          <input type="text" className="form-control bg-light" name="new-skill" value={newSkill} onChange={newSkillChange} />
+        <div className='col-sm-6 d-flex'>
+          <input type="text" className="form-control-sm bg-light" name="new-skill" value={newSkill} onChange={newSkillChange} />
           <button className="btn btn-secondary ms-2" onClick={AddItem}>Save</button>
         </div>
       </>
--- src/scripts/resume-builder/WorkExperienceFormItem.js
+++ src/scripts/resume-builder/WorkExperienceFormItem.js
@@ -1,78 +1,95 @@
 import { useRef, useState } from 'react'
 import React from 'react';
 
-function WorkExperienceFormItem(){
-    const [formData, setFormData] = useState({
-        companyName: '',
-        jobTitle: '',
-        city: '',
-        location: '',
-        from: '',
-        to: '',
-        description: '',
-        current: false
-    });
+function WorkExperienceFormItem( { item: formData, resume: [resume, setResume], idx} ){
 
     const handleChange = (e) => {
       let { name, value, checked } = e.target;
       if(e.target.type === "checkbox"){
         value = checked;
       }
-      setFormData(prevState => ({
-          ...prevState,
-          [name]: value
+
+      let newWork = [...resume.work];
+      newWork[idx] = {
+        ...newWork[idx],
+        [name]: value
+      }
+      setResume(prevState => ({
+        ...prevState,
+        work: newWork
       }));
 
     };
 
-    const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
+    function move(e){
+      e.preventDefault();
+      const dir = e.target.dataset.dir === "up" ? -1 : 1;
 
-    setFormData({
-        companyName: '',
-        jobTitle: '',
-        city: '',
-        location: '',
-        from: '',
-        to: '',
-        description: '',
-        current: false
-    });
-    };
+      setResume(prevState => {
+        const items = [...prevState.work];
+        const holder = prevState.work[idx];
+        items[idx] = items[idx + dir];
+        items[idx + dir] = holder;
+        return {
+          ...prevState,
+          work: items
+        }
+      })
+    }
+
+    function remove(e){
+      e.preventDefault();
+
+      setResume(prevState => {
+        const items = [...prevState.work];
+        
+        return {
+          ...prevState,
+          work: items.slice(0, idx).concat(items.slice(idx+1))
+        }
+      })
+    }
+
 
     return (
-    <form onSubmit={handleSubmit} className='content p-2 my-1'>
+    <form className='content p-2 my-1 resume-form'>
       <div className="row mb-3">
-        <div className="col-sm-6 form-floating">
-          <input type="text" className="form-control ps-2 bg-light" placeholder='_' id="companyName" name="companyName" value={formData.companyName} onChange={handleChange} />
-            <label htmlFor="companyName" className="ms-2 col-form-label">Company name *</label>
+        <div className="col-sm-4">
+          <label htmlFor="companyName" className="position-absolute invisible col-form-label">Employer</label>
+          <input type="text" className="h-75 form-control-sm ps-2 bg-light" placeholder='Employer' id="companyName" name="companyName" value={formData.companyName} onChange={handleChange} />
         </div>
-        <div className="col-sm-6 form-floating">
-          <input type="text" className="form-control ps-2 bg-light" placeholder='_' id="jobTitle" name="jobTitle" value={formData.jobTitle} onChange={handleChange} />
-            <label htmlFor="jobTitle" className="ms-2 col-form-label">Job title *</label>
+        <div className="col-sm-4">
+            <label htmlFor="jobTitle" className="position-absolute invisible col-form-label">Job Title</label>
+          <input type="text" className="h-75 form-control-sm ps-2 bg-light" placeholder='Job Title' id="jobTitle" name="jobTitle" value={formData.jobTitle} onChange={handleChange} />
+        </div>
+        <div className="col-sm-4 d-flex justify-content-end mb-3">
+          <div className="btn-group" role="group" >
+            <button onClick={move} data-dir="up" className={`btn btn-secondary ${idx == 0 ? 'disabled' : ''}`}><i className="fa-solid fa-arrow-up"></i></button>
+            <button onClick={move} data-dir="down" className={`btn btn-secondary ${idx == resume.work.length-1 ? 'disabled' : ''}`}><i className="fa-solid fa-arrow-down"></i></button>
+            <button onClick={remove} className={`btn btn-danger ${resume.work.length == 1 ? 'disabled' : ''}`}><i className="fa-solid fa-trash-can"></i></button>
+          </div>
         </div>
       </div>
 
-      <div className="row mb-3">
-        <div className="col-sm-6 form-floating">
-          <input type="tel" className="form-control ps-2 bg-light" placeholder='_' id="city" name="city" value={formData.city} onChange={handleChange} />
-            <label htmlFor="city" className="ms-2 col-form-label">Location</label>
+      <div className="row mb-3 me-5">
+        <div className="col-sm-4">
+          <label htmlFor="city" className="position-absolute invisible col-form-label">Location</label>
+          <input type="tel" className="form-control-sm ps-2 bg-light" placeholder='Location' id="city" name="city" value={formData.city} onChange={handleChange} />
         </div>
-        <div className="col-sm-6 form-floating">
-          <input type="date" className="form-control ps-2 bg-light" placeholder='_' id="from" name="from" value={formData.from} onChange={handleChange} />
-            <label htmlFor="from" className="ms-2 col-form-label">From</label>
+        <div className="d-flex justify-content-end col-sm-6">
+          <label htmlFor="from" className="me-2 col-form-label">From</label>
+          <input type="date" className="form-control-sm ps-2 bg-light" id="from" name="from" value={formData.from} onChange={handleChange} />
         </div>
       </div>
 
-      <div className="row mb-3">
-        <div className="col-sm-6 d-flex align-items-center" >
+      <div className="row mb-3 me-5">
+        <div className="col-sm-4 d-flex align-items-center" >
           <label htmlFor="current" className="ms-2 col-form-label">I currently work here </label>
           <input type="checkbox" className="ms-2 bg-light" id="current" name="current" checked={formData.current} value={formData.current} onChange={handleChange} />
         </div>
-        <div className="col-sm-6 form-floating hideable">
-          <input type="date" className="form-control ps-2 bg-light" placeholder='_' id="to" name="to" value={formData.to} onChange={handleChange} disabled={formData.current} />
-            <label htmlFor="to" className="ms-2 col-form-label">To</label>
+        <div className="d-flex justify-content-end col-sm-6 hideable">
+          <label htmlFor="to" className="me-2 col-form-label">To</label>
+          <input type="date" className="form-control-sm ps-2 bg-light" id="to" name="to" value={formData.to} onChange={handleChange} disabled={formData.current} />
         </div>
       </div>
 
@@ -83,7 +100,7 @@ function WorkExperienceFormItem(){
         </div>
       </div>
       <div className="row mb-3">
-        <div className="col-sm-10 form-floating">
+        <div className="col-sm-10">
           <button className="btn btn-primary">Generate with AI</button>
         </div>
       </div>
