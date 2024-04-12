95a61e3eb3a6a24cc8f78f2e84757b53ae506556
cjsmi <ClarenceSmith90@gmail.com>
2024-04-12 15:56:37 -0400

    action fix v3

--- test/controllers/bug_reports_controller_test.rb
+++ test/controllers/bug_reports_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class BugReportsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get bug_reports_create_url
+  test "should get new" do
+    get bug_report_url
     assert_response :success
   end
 end
