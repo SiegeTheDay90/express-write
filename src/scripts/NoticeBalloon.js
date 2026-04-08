4dac794b54c1a03e2bd3461d283055cdc6e77f47
siegetheday90 <ClarenceSmith90@gmail.com>
2026-04-08 11:05:09 -0400

    add accessibility to NoticeBalloon

--- src/scripts/NoticeBalloon.js
+++ src/scripts/NoticeBalloon.js
@@ -4,9 +4,11 @@ class NoticeBalloon{
         this.parent = container;
         this.balloon = document.createElement("div");
         this.balloon.style.position = "relative";
-        this.balloon.classList.add("balloon")
-        this.balloon.classList.add(this.type === "error" ? "text-danger" : "text-info")
+        this.balloon.classList.add("balloon");
+        this.balloon.classList.add(this.type === "error" ? "text-danger" : "text-info");
         this.balloon.classList.add(type);
+        this.balloon.setAttribute("role", this.type === "error" ? "alert" : "status");
+        this.balloon.setAttribute("aria-live", "assertive");
         this.balloon.innerText = message;
         if(this.type === "error"){
             const reportButton = document.createElement("span");
@@ -16,11 +18,13 @@ class NoticeBalloon{
             reportButton.style.bottom = "4px";
             this.balloon.appendChild(reportButton);
         }
-        this.closeButton = document.createElement("span");
-        this.balloon.appendChild(this.closeButton);
+        // this.closeButton = document.createElement("span");
+        this.closeButton = document.createElement("button");
+        this.closeButton.setAttribute("aria-label", "Dismiss notification");
         this.closeButton.classList.add("close-button");
+        this.balloon.appendChild(this.closeButton);
         this.close = this.close.bind(this);
-        this.closeButton.addEventListener('click', this.close)
+        this.closeButton.addEventListener('click', this.close);
         this.opacity = 1;
         this.parent.appendChild(this.balloon);
     }
